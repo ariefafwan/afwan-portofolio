fde7b2a897486b730ee4e6b9cf1780ff8fa771f0
Afwan <tmariefafwan@gmail.com>
2023-05-30 18:37:44 +0700

    blade layouts control

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="Afwan" />
+    <title>Arief Afwan</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="{{ asset ('favicon/favicon.ico') }}" />
+    <!-- Custom Google font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+    <!-- Bootstrap icons-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+    <!-- Core theme CSS & Bootstrap-->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+      crossorigin="anonymous"
+    />
+    <link href="{{ asset('assets/css/styles.css') }}" rel="stylesheet" />
+  </head>
+  <body class="d-flex flex-column h-100">
+    <main class="flex-shrink-0">
+        @include('layouts.nav')
+        @yield('welcome')
+    </main>
+    @include('layouts.footer')
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+                crossorigin="anonymous"></script>
+        <!-- Core theme JS-->
+        <script src="{{ asset ('assets/js/scripts.js') }}"></script>
+  </body>
+</html>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,30 @@
+<!-- Navigation-->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" style="background-color: aliceblue">
+    <div class="container px-3">
+      <a class="navbar-brand fw-bolder text-primary text-gradient" href="index.html">
+        <img src="{{ asset ('assets/img/logo.png') }}" alt="Logo" width="30" height="40" class="align-text-top" />
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarcontent" aria-controls="navbarcontent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarcontent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 small">
+          <li class="nav-item">
+            <a class="nav-link fw-bolder text-primary" href="index.html">HOME</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bolder text-primary" href="#about">ABOUT</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bolder text-primary" href="resume.html">BLOG</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bolder text-primary" href="projects.html">PROJECTS</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link fw-bolder text-primary" href="#contact">CONTACT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
